4eba3926d7af44f2c8f5e4e2ac3bc9d75dbfe058
Mario Vellandi <mvellandi@hotmail.com>
2021-11-15 01:09:12 +0100

    pass change

--- config/config.exs
+++ config/config.exs
@@ -2,5 +2,5 @@ import Config
 
 config :bolt_sips, Bolt,
   url: 'neo4j+s://e0c2e18b.databases.neo4j.io:7687',
-  basic_auth: [username: "neo4j", password: "3j3m0gzaWgViYXKi72_8TOU1_Yei300klrIki-raCqQ"],
+  basic_auth: [username: "neo4j", password: "pass"],
   ssl: true
