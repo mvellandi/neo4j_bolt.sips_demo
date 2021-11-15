1274c118af4f81f125347cefad4433e5d0ece6b6
Mario Vellandi <mvellandi@hotmail.com>
2021-11-15 01:10:28 +0100

    address change

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,6 @@
 import Config
 
 config :bolt_sips, Bolt,
-  url: 'neo4j+s://e0c2e18b.databases.neo4j.io:7687',
+  url: 'neo4j+s://address.databases.neo4j.io:7687',
   basic_auth: [username: "neo4j", password: "pass"],
   ssl: true
