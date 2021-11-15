7e0dc682a22ea46e411364cb444d558c0f46d855
Mario Vellandi <mvellandi@hotmail.com>
2021-11-15 12:42:19 +0100

    moved db env variable to runtime.exs

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,8 @@
-import Config
+# How runtime.exs config is used
+#
+# import Config
 
-config :bolt_sips, Bolt,
-  url: 'neo4j+s://address.databases.neo4j.io:7687',
-  basic_auth: [username: "neo4j", password: "pass"],
-  ssl: true
+# config :bolt_sips, Bolt,
+#   url: 'neo4j+s://address.databases.neo4j.io:7687',
+#   basic_auth: [username: "neo4j", password: "pass"],
+#   ssl: true
